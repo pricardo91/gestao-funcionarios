2a9c1d3ff2436fb941b09b810add37b1440de2e7
Ricardo Silva <pricardo.sribeiro@gmail.com>
2026-02-27 00:30:59 -0300

    docs: adicao de documentacao OpenAPI com Swagger

--- src/main/java/br/com/first/gestaofuncionarios/api/controller/FuncionarioController.java
+++ src/main/java/br/com/first/gestaofuncionarios/api/controller/FuncionarioController.java
@@ -5,7 +5,15 @@ import br.com.first.gestaofuncionarios.api.dto.FuncionarioResponse;
 import br.com.first.gestaofuncionarios.api.dto.FuncionarioUpdateRequest;
 import br.com.first.gestaofuncionarios.api.mapper.FuncionarioMapper;
 import br.com.first.gestaofuncionarios.domain.Funcionario;
+import br.com.first.gestaofuncionarios.handler.ErrorApiResponse;
 import br.com.first.gestaofuncionarios.service.FuncionarioService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -18,11 +26,27 @@ import java.util.stream.Collectors;
 @RestController
 @RequestMapping("/v1/funcionarios")
 @RequiredArgsConstructor
+@Tag(name = "Funcionários",
+        description = "Com os endpoints abaixo é possível cadastrar, atualizar, consultar e deletar funcionários")
 public class FuncionarioController {
 
     private final FuncionarioService funcionarioService;
     private final FuncionarioMapper funcionarioMapper;
 
+    @Operation(
+            summary = "Cadastrar funcionário",
+            description = "Cria um novo funcionário no sistema. Não permite telefone duplicado."
+    )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Funcionário criado com sucesso",
+                    content = @Content(schema = @Schema(implementation = FuncionarioResponse.class))),
+            @ApiResponse(responseCode = "400", description = "Dados inválidos",
+            content = @Content(schema = @Schema(implementation = ErrorApiResponse.class))),
+            @ApiResponse(responseCode = "409", description = "Telefone já cadastrado",
+                    content = @Content(schema = @Schema(implementation = ErrorApiResponse.class))),
+            @ApiResponse(responseCode = "500", description = "Erro interno no servidor",
+                    content = @Content(schema = @Schema(implementation = ErrorApiResponse.class)))
+    })
     @PostMapping
     public ResponseEntity<FuncionarioResponse> criar(@RequestBody @Valid FuncionarioCreateRequest funcionarioNovo) {
         Funcionario funcionario = funcionarioMapper.toDomain(funcionarioNovo);
@@ -31,6 +55,18 @@ public class FuncionarioController {
         return ResponseEntity.status(HttpStatus.CREATED).body(funcionarioResponse);
     }
 
+    @Operation(
+            summary = "Buscar funcionário por ID",
+            description = "Retorna os dados de um funcionário cadastrado com base no ID informado."
+    )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Funcionário encontrado",
+                    content = @Content(schema = @Schema(implementation = FuncionarioResponse.class))),
+            @ApiResponse(responseCode = "404", description = "Funcionário não encontrado",
+                    content = @Content(schema = @Schema(implementation = ErrorApiResponse.class))),
+            @ApiResponse(responseCode = "400", description = "ID inválido",
+                    content = @Content(schema = @Schema(implementation = ErrorApiResponse.class)))
+    })
     @GetMapping("/{id}")
     public ResponseEntity<FuncionarioResponse> buscarPorId(@PathVariable Long id) {
         Funcionario funcionario = funcionarioService.buscarPorId(id);
@@ -39,6 +75,15 @@ public class FuncionarioController {
                 .body(funcionarioMapper.toResponse(funcionario));
     }
 
+    @Operation(
+            summary = "Busca todos os funcionários",
+            description = "Busca a lista completa de funcionários cadastrados"
+    )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Lista de funcionários cadastrados",
+                    content = @Content(array = @ArraySchema(schema = @Schema(implementation = FuncionarioResponse.class)))
+            )
+    })
     @GetMapping
     public ResponseEntity<Collection<FuncionarioResponse>> buscarTodos() {
         Collection<FuncionarioResponse> listaFuncionarios = funcionarioService.listarTodos()
@@ -51,10 +96,23 @@ public class FuncionarioController {
                 .body(listaFuncionarios);
     }
 
+    @Operation(
+            summary = "Atualiza funcionário por ID",
+            description = "Atualiza os dados de um funcionário cadastrado com base no ID informado."
+    )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Funcionário atualizado com sucesso",
+                    content = @Content(schema = @Schema(implementation = FuncionarioResponse.class))),
+            @ApiResponse(responseCode = "400", description = "ID inválido",
+                    content = @Content(schema = @Schema(implementation = ErrorApiResponse.class))),
+            @ApiResponse(responseCode = "404", description = "Funcionário não encontrado",
+                    content = @Content(schema = @Schema(implementation = ErrorApiResponse.class))),
+            @ApiResponse(responseCode = "500", description = "Erro interno no servidor",
+                    content = @Content())
+    })
     @PutMapping("/{id}")
     public ResponseEntity<FuncionarioResponse> atualizar(@PathVariable Long id,
                                                          @RequestBody @Valid FuncionarioUpdateRequest request) {
-
         Funcionario funcionario = funcionarioMapper.toDomain(id, request);
 
         return ResponseEntity
@@ -62,6 +120,14 @@ public class FuncionarioController {
                 .body(funcionarioMapper.toResponse(funcionarioService.atualizar(id, funcionario)));
     }
 
+    @Operation(
+            summary = "Deleta funcionario por ID",
+            description = "Apaga um funcionario cadastrado com base no ID informado"
+    )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "Funcionário excluído com sucesso",
+                    content = @Content())
+    })
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deletar(@PathVariable Long id) {
         funcionarioService.deletar(id);
--- src/main/java/br/com/first/gestaofuncionarios/api/dto/FuncionarioCreateRequest.java
+++ src/main/java/br/com/first/gestaofuncionarios/api/dto/FuncionarioCreateRequest.java
@@ -1,29 +1,37 @@
 package br.com.first.gestaofuncionarios.api.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.*;
 import lombok.Data;
 
 import java.math.BigDecimal;
 
+@Schema(description = "Objeto de requisição para criação de funcionário")
 @Data
 public class FuncionarioCreateRequest {
 
+    @Schema(description = "Nome completo do funcionário", example = "Ricardo Silva")
     @NotBlank(message = "O nome é obrigatório")
     @Size(min = 3, max = 100, message = "O nome deve conter entre 3 e 100 carateres")
     private String nome;
 
+    @Schema(example = "Analista de Sistemas")
     @NotBlank(message = "O cargo do funcionário é obrigatório")
+    @Size(min = 3, max = 100, message = "A função deve conter entre 3 e 100 carateres")
     private String funcao;
 
+    @Schema(example = "3566.25")
     @NotNull(message = "O salário do funcionário é obrigatório")
     @Positive(message = "O salário deve ser positivo")
     @DecimalMin(value = "1621.00", message = "O valor não atinge o salário mínimo vigênte no Brasil (R$1.621,00)")
     private BigDecimal salario;
 
+    @Schema(description = "Número de telefone do usuário, não pode ser repetido!", example = "11982971929")
     @NotBlank(message = "Telefone é obrigatório")
     @Pattern(regexp = "\\d{10,11}", message = "Telefone deve conter 10 ou 11 dígitos")
     private String telefone;
 
+    @Schema(example = "Rua América Latina, 135 - São Paulo/SP")
     @NotBlank(message = "Endereço é obrigatório")
     @Size(min = 3, max = 250, message = "O endereço deve conter entre 3 à 250 caracteres!")
     private String endereco;
